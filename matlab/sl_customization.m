d9554cfc3091a4f4d251a873fd48268040f60375
Paul Prozesky <paulp@ska.ac.za>
2014-08-20 16:39:15 +0200

    Use the scripts in +helper... for menus.

--- matlab/sl_customization.m
+++ matlab/sl_customization.m
@@ -22,32 +22,32 @@ end
 function schema = userGetBlockSize(~)
     schema = sl_action_schema;
 	schema.label = 'Get block size';
-	schema.callback = @casper_sl_get_block_size; 
+	schema.callback = @helper_scripts.casper_sl_get_block_size; 
 end
 
 function schema = userSetBlockSize(~)
 	schema = sl_action_schema;
 	schema.label = 'Set block sizes';
-	schema.callback = @casper_sl_set_block_size; 
+	schema.callback = @helper_scripts.casper_sl_set_block_size; 
 end
 
 function schema = userGotoPlusPlus(~)
 	schema = sl_action_schema;
 	schema.label = 'Increment goto/from tags';
     schema.userdata = [-1, -1];
-	schema.callback = @casper_sl_incr_tag;
+	schema.callback = @helper_scripts.casper_sl_incr_tag;
 end
 
 function schema = userDannyFunc(~)
 	schema = sl_action_schema;
 	schema.label = 'Goto -> Goto++';
-	schema.callback = @casper_sl_goto_plusplus; 
+	schema.callback = @helper_scripts.casper_sl_goto_plusplus; 
 end
 
 function schema = userGoto2From(~)
 	schema = sl_action_schema;
 	schema.label = 'Goto -> From';
-	schema.callback = @casper_sl_goto2from; 
+	schema.callback = @helper_scripts.casper_sl_goto2from; 
 end
 
 % if you'd like to add more user functions duplicate 'userFunction1'
